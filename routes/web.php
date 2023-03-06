056bfc8112b548cc0c1cc143ca0dd608ddb53bb8
mhafidzp719 <mhafidzp719@gmail.com>
2023-03-06 17:03:33 +0700

    up template

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/layout', function () {
+    return view('layouts.index');
+});
 
 Auth::routes();
 
